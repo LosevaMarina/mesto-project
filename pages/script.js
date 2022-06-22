08bc9e7374f440d7d93838ca880fbe585c50f2b2
LosevaMarina <104833079+LosevaMarina@users.noreply.github.com>
2022-06-22 23:46:56 +0300

    commit

--- pages/script.js
+++ pages/script.js
@@ -1,6 +1,6 @@
 let buttonRedact = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
 function openPopup() {
-    popup.classList.add(".popup_active");
+    popup.classList.add('popup_active');
 }
 buttonRedact.addEventListener('click', openPopup);
