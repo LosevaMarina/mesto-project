b2bcb20564c0292041d3253f553048b3357b6bed
LosevaMarina <104833079+LosevaMarina@users.noreply.github.com>
2022-07-07 14:56:51 +0300

    2

--- pages/script.js
+++ pages/script.js
@@ -1,33 +1,23 @@
 let buttonRedact = document.querySelector('.profile__edit');
 let buttonClose = document.querySelector('.popup__close');
+let buttonAdd = document.querySelector('.profile__add');
 let popup = document.querySelector('.popup');
 let buttonSave = document.querySelector('.popup__save');
 
 //открыть popup
 function openPopup() {
     popup.classList.add('popup_active');
-    userName.value = 'Жак-Ив Кусто';
-    aboutMyself.value = 'Исследователь океана';
 }
 buttonRedact.addEventListener('click', openPopup);
-
-//закрыть popup
-function closePopup() {
-    popup.classList.remove('popup_active');
-}
-buttonClose.addEventListener('click', closePopup);
-
-//фуния сохранить форму
 
 //inputы, отправ формы
 const formElement = document.querySelector('.popup__fields');
 const userName = document.querySelector('#user_name');
 const aboutMyself = document.querySelector('#about_myself');
 
+//фуния сохранить форму
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    //userName.value;
-    //aboutMyself.value;
     let profileName = document.querySelector('.profile__name');
     let profileProf = document.querySelector('.profile__proffesion');
     profileName.textContent = userName.value;
@@ -37,3 +27,45 @@ function formSubmitHandler (evt) {
 formElement.addEventListener('submit', formSubmitHandler); 
 buttonSave.addEventListener('click', formSubmitHandler);
 buttonSave.addEventListener('click', closePopup);
+
+
+//закрыть popup
+function closePopup() {
+    popup.classList.remove('popup_active');
+}
+buttonClose.addEventListener('click', closePopup);
+
+
+//массив с рточми, добавляются при загруз страницы
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
+
+
+
+
+
