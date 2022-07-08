0c2c374be3a54a57d675449605d359639946b70c
LosevaMarina <104833079+LosevaMarina@users.noreply.github.com>
2022-07-08 09:36:05 +0300

    4com

--- pages/script.js
+++ pages/script.js
@@ -1,69 +1,4 @@
-//кнопка добавить  
-let buttonAdd = document.querySelector('.profile__add');
-//кнопка сохранить
-let buttonSave = document.querySelector('.popup__save');
-//кнопка закрыть 
-let buttonClose = document.querySelector('.popup__close');
-//кнопка редактировать
-let buttonRedact = document.querySelector('.profile__edit');
-
-let popups = document.querySelectorAll('.popup');
-let popupRedact = document.querySelector('.popup_red');
-let popupCard = document.querySelector('.popup_card');
-
-//функция открыть popup
-function openPopup(popup) {
-  popup.classList.add('popup_active');
-}
-//вешаем событие на открытие popup
-//buttonRedact.addEventListener('click', openPopup(popupRedact));
-//buttonAdd.addEventListener('click', openPopup(popupCard));
-buttonRedact.addEventListener('click', evt => {
-  openPopup(popupRedact);
-});
-
-buttonAdd.addEventListener('click', evt => {
-  openPopup(popupCard);
-});
-
-//функция закрыть popup
-function closePopup(popup) {
-  popup.classList.remove('popup_active');
-}
-
-buttonClose.addEventListener('click', evt => {
-  openPopup(popupCard);
-});
-
-//buttonClose.addEventListener('click', closePopup(popups));
-
-
-
-
-
-
-
-
-//inputы, отправ формы
-const formElement = document.querySelector('.popup__fields');
-const userName = document.querySelector('#user_name');
-const aboutMyself = document.querySelector('#about_myself');
-
-//фуния сохранить форму
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    let profileName = document.querySelector('.profile__name');
-    let profileProf = document.querySelector('.profile__proffesion');
-    profileName.textContent = userName.value;
-    profileProf.textContent = aboutMyself.value;
-    console.log(profileName);
-}
-formElement.addEventListener('submit', formSubmitHandler); 
-buttonSave.addEventListener('click', formSubmitHandler);
-buttonSave.addEventListener('click', closePopup);
-
-
-//массив с рточми, добавляются при загруз страницы
+//массив с карточки. добавляется при загрузке страницы
 const initialCards = [
     {
       name: 'Архыз',
@@ -90,5 +25,82 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ]; 
+
+//кнопка сохранить
+let buttonSave = document.querySelector('.popup__save');
+
+
+let popups = document.querySelectorAll('.popup');
+
+
+
+let buttonAdd = document.querySelector('.profile__add');//кнопка добавить
+buttonAdd.addEventListener('click', evt => { 
+    placeName.value ="";
+    placeLink.value ="";
+    openPopup(popupCard);
+});
+let buttonRedact = document.querySelector('.profile__edit');//кнопка редактировать
+buttonRedact.addEventListener('click', evt => { 
+    userName.value = profileName.textContent;
+    aboutMyself.value = profileProf.textContent;
+    openPopup(popupRedact);
+});
+
+//задаем переменные имя и профессия на странице
+let profileName = document.querySelector('.profile__name');
+let profileProf = document.querySelector('.profile__proffesion');
+
+//модалки
+let popupRedact = document.querySelector('.popup_red');
+let popupCard = document.querySelector('.popup_card');
+let buttonClose = document.querySelector('.popup__close');//кнопка закрыть 
+for (i = 0; i < buttonClose.length; i++)
+buttonClose[i].addEventListener('click', evt => {closePopup(evt.target.closest('.popup'));});
+
+//редактирование
+const userName = document.querySelector('#user_name');
+const aboutMyself = document.querySelector('#about_myself');
+const popupRedactForm = document.querySelector('.popup_red form');
+function submitRedactFormHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = userName.value;
+    profileProf.textContent = aboutMyself.value;
+    console.log(profileName);
+}
+popupRedactForm.addEventListener('submit', evt => {
+    submitRedactFormHandler(evt);
+    closePopup(popupRedact); 
+});
+
+//функция открыть popup
+function openPopup(popup) {
+  popup.classList.add('popup_active');
+}
+
+//функция закрыть popup
+function closePopup(popup) {
+  popup.classList.remove('popup_active');
+}
+
+//вставка изначальных карточек
+const cardsContainer = document.querySelector('.elements');
+for(i = 0; i < initialCards.length; i++)
+  cardsContainer.appendChild(getCardElement(initialCards[i].name, initialCards[i].link));
+  
+  initLinks();
+
+
+
+
+buttonClose.addEventListener('click', evt => {
+  openPopup(popupCard);
+});
+
+//buttonClose.addEventListener('click', closePopup(popups));
+
+
+
+
 
 
