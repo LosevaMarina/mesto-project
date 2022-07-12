a6beb30ce3a78e3593cf291288314b7717a1755f
LosevaMarina <104833079+LosevaMarina@users.noreply.github.com>
2022-07-12 03:11:17 +0300

    comit-xz

--- pages/script.js
+++ pages/script.js
@@ -26,33 +26,6 @@ const initialCards = [
     }
   ]; 
 
-//кнопка редактировать
-const buttonRedact = document.querySelector('.profile__edit');
-buttonRedact.addEventListener('click', evt => { 
-    userName.value = profileName.textContent;
-    aboutMyself.value = profileProf.textContent;
-    openPopup(popupRedact);
-});
-
-//кнопка добавить
-const buttonAdd = document.querySelector('.profile__add');
-buttonAdd.addEventListener('click', evt => { 
-    //placeName.value ="";
-    //placeLink.value ="";
-    document.querySelector('#add_card_form').reset();
-    openPopup(popupCard);
-});
-
-//функция открыть popup
-function openPopup(popup) {
-  popup.classList.add('popup_active');
-}
-
-//функция закрыть popup
-function closePopup(popup) {
-  popup.classList.remove('popup_active');
-}
-
 //задаем переменные имя и профессия на странице
 const profileName = document.querySelector('.profile__name');
 const profileProf = document.querySelector('.profile__proffesion');
@@ -64,69 +37,89 @@ const popupPhoto = document.querySelector('.popup_photo');
 const popupImage = document.querySelector(".popup__figure-img");
 const popupImageName = document.querySelector(".popup__figure-desc");
 const popupPhotoFullSize = document.querySelector(".popup_photo");
+const popupLike = document.querySelector('.element__like');
+
+
+const cardElement = document.querySelector('.cards-template').content;
+
+//редактирование формы 
+const userName = document.querySelector('#user_name');
+const aboutMyself = document.querySelector('#about_myself');
+const popupRedactForm = document.querySelector('.popup_red form');
+//добавление новой картинки
+const placeName = document.querySelector('#about_card');
+const placeLink = document.querySelector('#link');
+const popupCardForm = document.querySelector(".popup_card form");
+
+
+
+
+
+//кнопка редактировать
+const buttonRedact = document.querySelector('.profile__edit');
+buttonRedact.addEventListener('click', evt => { 
+    userName.value = profileName.textContent;
+    aboutMyself.value = profileProf.textContent;
+    openPopup(popupRedact);
+});
+
+//кнопка добавить
+const buttonAdd = document.querySelector('.profile__add');
+buttonAdd.addEventListener('click', evt => { 
+    popupCardForm.reset();
+    openPopup(popupCard);
+});
+
+//функция открыть popup
+function openPopup(popup) {
+  popup.classList.add('popup_active');
+};
+
+//функция закрыть popup
+function closePopup(popup) {
+  popup.classList.remove('popup_active');
+}
 
 //кнопка закрыть 
 const closeButtonList= document.querySelectorAll('.popup__close');
 for (let i = 0; i < closeButtonList.length; i++)
 closeButtonList[i].addEventListener('click', evt => { closePopup(evt.target.closest(".popup")); });
 
-//редактирование формы 
-const userName = document.querySelector('#user_name');
-const aboutMyself = document.querySelector('#about_myself');
-const popupRedactForm = document.querySelector('.popup_red form');
+//функция редактирование формы 
 function submitRedactFormHandler(evt) {
   evt.preventDefault();
   profileName.textContent = userName.value;
   profileProf.textContent = aboutMyself.value;
 }
-popupRedactForm.addEventListener('submit', evt => {
-  submitRedactFormHandler(evt);
-  closePopup(popupRedact);
-});
 
 //вставка изначальных карточек
 const cardsContainer = document.querySelector(".elements");
 for (let i = 0; i < initialCards.length; i++)
   cardsContainer.append(createCardElement(initialCards[i].name, initialCards[i].link));
 
-//добавление новой картинки
-const placeName = document.querySelector('#about_card');
-const placeLink = document.querySelector('#link');
-const popupCardForm = document.querySelector(".popup_card form");
+//функция добавления новой картинки
 function submitCardFormHandler(evt) {
   evt.preventDefault();
   cardsContainer.prepend(createCardElement(placeName.value, placeLink.value));
 }
+
+//слушатели кнопок
+popupRedactForm.addEventListener('submit', evt => {
+  submitRedactFormHandler(evt);
+  closePopup(popupRedact);
+});
 
 popupCardForm.addEventListener('submit', evt => {
   submitCardFormHandler(evt);
   closePopup(popupCard);
 });
 
+
 function createCardElement(name, link) {
-
-  const CardElement = document.querySelector('.cards-template').content;
-  const cardEl = CardElement.querySelector('.element').cloneNode(true);
-
+  const cardEl = cardElement.querySelector('.element').cloneNode(true);
   cardEl.querySelector('.element__title').textContent = name;
   cardEl.querySelector('.element__photo').alt = name;
   cardEl.querySelector('.element__photo').src = link;
-  cardsContainer.append(cardEl);
-
-  //let cardEl = document.createElement("article");
-  //cardEl.setAttribute("class", "element");
-  //cardEl.innerHTML = `
-  //    <img src="" alt="" class="element__photo">
-  //    <button class="element__delete" type="button"></button>
-  //    <div class="element__title-like">
-  //        <h2 class="element__title"></h2>
-  //        <button class="element__like" type="button" aria-label="поставить нравится">
-  //        </button>
-  //    </div>`;
-  //cardEl.querySelector("img").setAttribute("src", link);
-  //cardEl.querySelector("img").setAttribute("alt", name);
-  //cardEl.querySelector("h2").textContent = name;
-
 
   //Лайк
   cardEl.querySelector(".element__like").onclick = (evt) => {
